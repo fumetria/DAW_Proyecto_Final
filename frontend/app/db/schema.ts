76742ab9f5248f446f8289a38dae79645c585500
Fu Jun <fulin.fujun@outlook.es>
2025-12-28 00:43:11 +0100

    Add categories and articles schema

--- frontend/app/db/schema.ts
+++ frontend/app/db/schema.ts
@@ -1,7 +1,9 @@
-import { uuid, pgTable, varchar, boolean } from "drizzle-orm/pg-core";
+import { uuid, pgTable, varchar, boolean, real, integer } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
     id: uuid().defaultRandom().primaryKey(),
+    email: varchar({ length: 255 }).notNull().unique(),
+    password: varchar({ length: 255 }).notNull(),
     dni: varchar({ length: 12 }).notNull().unique(),
     name: varchar({ length: 255 }).notNull(),
     surname1: varchar({ length: 255 }).notNull(),
@@ -11,3 +13,16 @@ export const usersTable = pgTable("users", {
     organization: varchar({ length: 255 }),
     is_active: boolean().default(true),
 });
+
+export const categoriesTable = pgTable("categories", {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    name: varchar({ length: 255 }).notNull().unique(),
+})
+
+export const articlesTable = pgTable("articles", {
+    id: uuid().defaultRandom().primaryKey(),
+    cod_art: varchar({ length: 255 }).notNull().unique(),
+    name: varchar({ length: 255 }).notNull(),
+    category: integer('category_id'),
+    pvp: real().notNull(),
+})
