d9e2f5bab5781d2ac7895f26514952a83c89a396
Fu Jun <fulin.fujun@outlook.es>
2025-12-29 22:28:05 +0100

    Update Drizzle orm example

--- frontend/app/index.ts
+++ frontend/app/index.ts
@@ -1,8 +1,8 @@
 import bcrypt from 'bcrypt';
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
-import { usersTable } from './db/schema';
-import { eq } from 'drizzle-orm';
+import { articlesTable, categoriesTable, usersTable } from './db/schema';
+import { eq, sql } from 'drizzle-orm';
 
 
 const db = drizzle(process.env.DATABASE_URL!);
@@ -37,4 +37,80 @@ async function main() {
     console.log('User deleted!');
 }
 
-main();
+async function articlesExample() {
+
+    const categories = ["books", "food"];
+
+    await Promise.all(
+        categories.map(async (category) => {
+            try {
+                const category1: typeof categoriesTable.$inferInsert = {
+                    name: category,
+                }
+                await db.insert(categoriesTable).values(category1);
+            } catch (error) {
+                console.error('Error: ', error)
+            }
+        })
+    )
+
+
+    const categoriesData = await db.select().from(categoriesTable);
+    console.log('Categories list: ', categoriesData);
+
+    const articles = [
+        {
+            name: 'mybook',
+            cod_art: 'a01',
+            category: 'books',
+            price: 15.00,
+        }
+        ,
+
+        {
+            name: 'mybike',
+            cod_art: 'b01',
+            category: 'bikes',
+            price: 100,
+        }
+    ];
+
+    await Promise.all(
+        articles.map(async (article) => {
+            try {
+                const articleCategory = await db.select().from(categoriesTable).where(eq(categoriesTable.name, article.category));
+                //articleCategory es un array, por tanto, para acceder al valor hay que seleccionar el elemento 0 del array
+                if (articleCategory.length) {
+                    console.log(`Category selected:\n id: ${articleCategory[0].id}\n name: ${articleCategory[0].name}`);
+                }
+
+                if (!articleCategory.length) {
+                    console.error(`Category '${article.category}' not found`);
+                } else {
+                    const newArticle: typeof articlesTable.$inferInsert = {
+                        name: article.name,
+                        cod_art: article.cod_art,
+                        category: articleCategory[0].id,
+                        pvp: article.price,
+                    }
+                    await db.insert(articlesTable).values(newArticle);
+                    console.log('Article inserted');
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        })
+    )
+
+
+    const articlesList = await db.select().from(articlesTable);
+    console.log('Articles list: ', articlesList);
+
+    await db.execute(sql`TRUNCATE TABLE categories RESTART IDENTITY CASCADE;`);
+    await db.execute(sql`TRUNCATE TABLE articles;`);
+
+
+}
+
+// main();
+articlesExample();
