f00add0036a95bba639e4fa4b441f32f63952c1d
Fu Jun <fulin.fujun@outlook.es>
2025-12-31 00:54:17 +0100

    Improve Header layout and Login button

--- frontend/app/components/Header.tsx
+++ frontend/app/components/Header.tsx
@@ -1,16 +1,37 @@
-import Image from "next/image"
+import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
 
-export default function Header(){
-    return(
-        <>
-        <header className="bg-stone-100 border-b border-stone-400">
-            <div>
-                <Image src={'/next.svg'} alt="Bsness app logo" width={3} height={3}/>
-                <h1 className="text-stone-950 xl:text-4xl">Bsness App</h1>
-            </div>
-            <div></div>
-            <div></div>
-        </header>
-        </>
-    )
+export default function Header() {
+  return (
+    <>
+      <header className="bg-stone-100 border-b border-stone-400 py-1 dark:bg-slate-800 dark:border-slate-500 w-screen">
+        <section className="flex justify-between items-center mx-5 ">
+          <div className="flex items-center gap-2">
+            <Image
+              src="/vercel.svg"
+              alt="Bsness app logo"
+              width={30}
+              height={30}
+              className=""
+            />
+            <h1 className="text-stone-950 dark:text-slate-50 xl:text-4xl">
+              Bsness App
+            </h1>
+          </div>
+          <div></div>
+          <div className="">
+            <Link
+              href={"/login"}
+              className="flex gap-1 rounded bg-blue-600 dark:bg-cyan-500 dark:hover:bg-cyan-200 text-slate-50 dark:hover:text-cyan-500 hover:cursor-pointer px-2 py-1 items-center font-semibold"
+            >
+              <FontAwesomeIcon icon={faUser} size="lg" className="" />
+              <p>Acceder</p>
+            </Link>
+          </div>
+        </section>
+      </header>
+    </>
+  );
 }
