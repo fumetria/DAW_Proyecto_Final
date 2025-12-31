d1905b68058ca3c88351c71309104976b98e998a
Fu Jun <fulin.fujun@outlook.es>
2025-12-31 22:17:40 +0100

    Add dashboad/tpv route

---
+++ frontend/app/dashboard/tpv/page.tsx
@@ -0,0 +1,9 @@
+import TpvInterface from "@/app/ui/pos-interface/TpvInterface";
+
+export default function Page() {
+  return (
+    <main className="h-screen w-full">
+      <TpvInterface />
+    </main>
+  );
+}
