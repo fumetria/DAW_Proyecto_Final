2f047b17531db9b185835a689c637be0fe36f170
Fu Jun <fulin.fujun@outlook.es>
2026-01-08 23:50:13 +0100

    Add new functions for fetching articles and categories from db

--- frontend/app/lib/data.ts
+++ frontend/app/lib/data.ts
@@ -1,21 +1,44 @@
+'use server'
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import * as schema from '@/app/db/schema';
+import { sql } from 'drizzle-orm';
+
 
 const db = drizzle(process.env.DATABASE_URL!, { schema });
 
 export async function fetchArticlesCategories() {
     try {
-        const categories = await db.select().from(schema.categoriesTable);
-        return categories;
+        const result = await db.select().from(schema.categoriesTable);
+        return result;
     } catch (error) {
         console.error('Something go wrong...', error);
+    }
+}
+
+export async function fetchCategoryById(id: string) {
+    try {
+        const category = await db.select().from(schema.categoriesTable).where(sql`${schema.categoriesTable.id} = ${id}`);
+        return category;
+    } catch (error) {
+        console.error(error);
     }
 }
 
 export async function fetchArticles() {
     try {
         const articles = await db.select().from(schema.articlesTable);
+        return articles;
+    } catch (error) {
+        console.error('Something go wrong...', error);
+    }
+}
+
+export async function fetchArticlesByCategory(category: string) {
+    try {
+        const categoryFetch = await db.select().from(schema.categoriesTable).where(sql`${schema.categoriesTable.name} = ${category}`);
+        if (!categoryFetch.length) return [];
+        const articles = await db.select().from(schema.articlesTable).where(sql`${schema.articlesTable.category} = ${categoryFetch[0].id}`);
         return articles;
     } catch (error) {
         console.error('Something go wrong...', error);
