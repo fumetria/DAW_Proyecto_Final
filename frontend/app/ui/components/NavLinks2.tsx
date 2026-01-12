dc20c0b6345812d44c4abc8865d9cad342813c62
Fu Jun <fulin.fujun@outlook.es>
2026-01-12 17:30:21 +0100

    Change key value of Link component to <li> element

--- frontend/app/ui/components/NavLinks2.tsx
+++ frontend/app/ui/components/NavLinks2.tsx
@@ -30,9 +30,8 @@ export default function NavLinks2() {
         if (link.type == "link") {
           return (
             <>
-              <li>
+              <li key={link.name}>
                 <Link
-                  key={link.name}
                   href={link.href}
                   className={clsx(
                     "flex h-12 grow items-center justify-center gap-2 rounded-md  p-3 text-sm font-semibold  md:flex-none md:justify-start md:p-2 md:px-3",
