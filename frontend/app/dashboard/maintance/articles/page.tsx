f3f9113ce9623703cec860a5bf2625f2cc12e4dd
Fu Jun <fulin.fujun@outlook.es>
2026-01-12 17:59:45 +0100

    Add Roboto flex in article page tittle

--- frontend/app/dashboard/maintance/articles/page.tsx
+++ frontend/app/dashboard/maintance/articles/page.tsx
@@ -1,5 +1,6 @@
 import ArticlesTable from "@/app/ui/dashboard/articles/table";
 import { Metadata } from "next";
+import { robotoFlex } from "@/app/fonts";
 
 export const metadata: Metadata = {
   title: "Dashboard Articles",
@@ -10,7 +11,9 @@ export default function Page() {
     <>
       <section className="w-full">
         <div className="mb-5">
-          <h1 className="text-2xl 2xl:text-4xl font-semibold dark:text-slate-50">
+          <h1
+            className={`${robotoFlex.className}text-2xl 2xl:text-4xl font-semibold dark:text-slate-50`}
+          >
             Mantenimiento artículos
           </h1>
         </div>
