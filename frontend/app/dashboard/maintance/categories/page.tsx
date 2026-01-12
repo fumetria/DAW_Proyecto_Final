793ef8b00bcea71281eaec3fd328327187dcde8d
Fu Jun <fulin.fujun@outlook.es>
2026-01-12 16:24:32 +0100

    Create user and categories page inside maintance subroute

--- frontend/app/dashboard/maintance/categories/page.tsx
+++ frontend/app/dashboard/maintance/categories/page.tsx
@@ -1,7 +1,8 @@
+import ArticlesTable from "@/app/ui/dashboard/articles/table";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Dashboard Mantenimiento Usuarios",
+  title: "Dashboard Mantenimiento Categorías",
 };
 
 export default function Page() {
@@ -10,9 +11,10 @@ export default function Page() {
       <section className="w-full">
         <div className="mb-5">
           <h1 className="text-2xl 2xl:text-4xl font-semibold dark:text-slate-50">
-            Mantenimiento usuarios
+            Mantenimiento categorías
           </h1>
         </div>
+        <ArticlesTable />
       </section>
     </>
   );
