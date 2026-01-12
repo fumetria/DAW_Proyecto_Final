465c9de3982cac5623def1699d692c3f8155315e
Fu Jun <fulin.fujun@outlook.es>
2026-01-12 22:29:00 +0100

    Add update article button

--- frontend/app/ui/dashboard/articles/buttons.tsx
+++ frontend/app/ui/dashboard/articles/buttons.tsx
@@ -1,14 +1,15 @@
-import { faPencilRuler } from "@fortawesome/free-solid-svg-icons";
+import { faPencil } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 
 export function UpdateArticle({ id }: { id: string }) {
   return (
     <Link
+      title="Actualizar articulo"
       href={`/dashboard/maintance/articles/${id}/edit`}
-      className="rounded-md dark:border p-2 bg-blue-600 hover:bg-gray-100 dark:bg-slate-800 dark:hover:bg-cyan-400 dark:hover:text-slate-500 dark:border-slate-500"
+      className="rounded-md dark:border p-2 bg-blue-600 text-stone-100 hover:bg-blue-200 hover:text-blue-600 dark:bg-slate-600 dark:hover:bg-cyan-400 dark:hover:text-slate-50 dark:border-slate-500"
     >
-      <FontAwesomeIcon icon={faPencilRuler} />
+      <FontAwesomeIcon icon={faPencil} />
     </Link>
   );
 }
