70856b038d63c43144feff70b1688950db85e5bb
Fu Jun <fulin.fujun@outlook.es>
2026-01-12 22:28:18 +0100

    Add icon attribute to navLink type "link" with the definition of IconDefinition from Fontawesome

--- frontend/app/lib/types/navigation.ts
+++ frontend/app/lib/types/navigation.ts
@@ -1,8 +1,11 @@
+import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
+
 export type navLink =
     {
         type: "link";
         name: string;
         href: string;
+        icon: IconDefinition;
     }
     | {
         type: "group";
