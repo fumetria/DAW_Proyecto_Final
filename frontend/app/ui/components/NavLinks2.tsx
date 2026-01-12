b840129fb0958d33f88ad9abdbee1395e5c888d8
Fu Jun <fulin.fujun@outlook.es>
2026-01-12 22:28:41 +0100

    Add icon for nav links

--- frontend/app/ui/components/NavLinks2.tsx
+++ frontend/app/ui/components/NavLinks2.tsx
@@ -5,11 +5,28 @@ import { usePathname } from "next/navigation";
 import clsx from "clsx";
 import { navLink } from "@/app/lib/types/navigation";
 import NavLinkDropDown2 from "./NavLinkDropdown2";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faDesktop,
+  faFileInvoiceDollar,
+  faHome,
+  faReceipt,
+} from "@fortawesome/free-solid-svg-icons";
 
 const links: navLink[] = [
-  { type: "link", name: "Home", href: "/dashboard" },
-  { type: "link", name: "Receipts", href: "/dashboard/receipts" },
-  { type: "link", name: "EoD", href: "/dashboard/end-day" },
+  { type: "link", name: "Home", href: "/dashboard", icon: faHome },
+  {
+    type: "link",
+    name: "Receipts",
+    href: "/dashboard/receipts",
+    icon: faReceipt,
+  },
+  {
+    type: "link",
+    name: "EoD",
+    href: "/dashboard/end-day",
+    icon: faFileInvoiceDollar,
+  },
   {
     type: "group",
     groupName: "Maintance",
@@ -19,7 +36,7 @@ const links: navLink[] = [
       { name: "Users", href: "/dashboard/maintance/users" },
     ],
   },
-  { type: "link", name: "TPV", href: "/tpv" },
+  { type: "link", name: "TPV", href: "/tpv", icon: faDesktop },
 ];
 
 export default function NavLinks2() {
@@ -32,8 +49,9 @@ export default function NavLinks2() {
             <li key={link.name} className="mt-6" title="Acceder a TPV">
               <Link
                 href={link.href}
-                className="flex h-12 items-center justify-center rounded-md bg-blue-600 p-3 text-sm font-semibold text-stone-100 hover:bg-blue-200 hover:text-blue-600 dark:bg-cyan-600 dark:text-slate-50 dark:hover:bg-cyan-200 dark:hover:text-cyan-600"
+                className="flex h-12 items-center justify-center gap-1 rounded-md bg-blue-600 p-3 text-base font-semibold text-stone-100 hover:bg-blue-200 hover:text-blue-600 dark:bg-cyan-600 dark:text-slate-50 dark:hover:bg-cyan-200 dark:hover:text-cyan-600"
               >
+                <FontAwesomeIcon icon={link.icon} />
                 <p className="hidden md:block">{link.name}</p>
               </Link>
             </li>
@@ -45,12 +63,13 @@ export default function NavLinks2() {
               <Link
                 href={link.href}
                 className={clsx(
-                  "flex h-12 grow items-center justify-center gap-2 rounded-md  p-3 text-sm font-semibold  md:flex-none md:justify-start md:p-2 md:px-3",
+                  "flex h-12 grow items-center justify-center gap-1 rounded-md  p-3 text-base font-semibold  md:flex-none md:justify-start md:p-2 md:px-3",
                   pathname === link.href
                     ? "bg-sky-100 text-blue-600 dark:bg-cyan-200 dark:text-cyan-600"
                     : "bg-stone-100 hover:bg-sky-100 hover:text-blue-600 dark:bg-slate-800 dark:text-slate-50 dark:hover:bg-cyan-100  dark:hover:text-cyan-500"
                 )}
               >
+                <FontAwesomeIcon icon={link.icon} />
                 <p className="hidden md:block">{link.name}</p>
               </Link>
             </li>
