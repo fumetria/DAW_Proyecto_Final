e1b60fd49e6c952e9045b0701eec7ae399d06425
Fu Jun <fulin.fujun@outlook.es>
2026-01-12 17:59:00 +0100

    Delete old navlinks components

--- frontend/app/ui/components/SideNav.tsx
+++ frontend/app/ui/components/SideNav.tsx
@@ -1,5 +1,3 @@
-import NavLinkDropDown from "./NavLinkDropdown";
-import NavLinks from "./NavLinks";
 import NavLinks2 from "./NavLinks2";
 
 export default function SideNav() {
@@ -7,8 +5,6 @@ export default function SideNav() {
     <>
       <div className="flex h-full flex-col px-3 py-4 md:px-2 bg-stone-100 dark:bg-slate-800">
         <ul>
-          {/* <NavLinks />
-          <NavLinkDropDown /> */}
           <NavLinks2 />
         </ul>
       </div>
