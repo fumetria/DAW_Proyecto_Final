283f5129e6514b322d818a6eff648198711f963c
Fu Jun <fulin.fujun@outlook.es>
2026-01-12 20:38:57 +0100

    Fix linter error

--- frontend/app/ui/components/NavLinks2.tsx
+++ frontend/app/ui/components/NavLinks2.tsx
@@ -41,21 +41,19 @@ export default function NavLinks2() {
         }
         if (link.type == "link") {
           return (
-            <>
-              <li key={link.name}>
-                <Link
-                  href={link.href}
-                  className={clsx(
-                    "flex h-12 grow items-center justify-center gap-2 rounded-md  p-3 text-sm font-semibold  md:flex-none md:justify-start md:p-2 md:px-3",
-                    pathname === link.href
-                      ? "bg-sky-100 text-blue-600 dark:bg-cyan-100 dark:text-cyan-600"
-                      : "bg-stone-100 hover:bg-sky-100 hover:text-blue-600 dark:bg-slate-800 dark:text-slate-50 dark:hover:bg-cyan-100  dark:hover:text-cyan-500"
-                  )}
-                >
-                  <p className="hidden md:block">{link.name}</p>
-                </Link>
-              </li>
-            </>
+            <li key={link.name}>
+              <Link
+                href={link.href}
+                className={clsx(
+                  "flex h-12 grow items-center justify-center gap-2 rounded-md  p-3 text-sm font-semibold  md:flex-none md:justify-start md:p-2 md:px-3",
+                  pathname === link.href
+                    ? "bg-sky-100 text-blue-600 dark:bg-cyan-200 dark:text-cyan-600"
+                    : "bg-stone-100 hover:bg-sky-100 hover:text-blue-600 dark:bg-slate-800 dark:text-slate-50 dark:hover:bg-cyan-100  dark:hover:text-cyan-500"
+                )}
+              >
+                <p className="hidden md:block">{link.name}</p>
+              </Link>
+            </li>
           );
         }
         return <NavLinkDropDown2 key={link.groupName} groupLinks={link} />;
