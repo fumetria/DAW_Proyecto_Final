20c80419cb787bf9c7b8c33e377ad396c7a2d03b
Fu Jun <fulin.fujun@outlook.es>
2026-01-12 17:59:18 +0100

    Add Roboto font as main font

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,20 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
+import { robotoSans, robotoFlex } from "./fonts";
 
 config.autoAddCss = false;
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: {
@@ -32,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-stone-300 dark:bg-slate-900`}
+        className={`${robotoSans.className} ${robotoFlex.className} antialiased bg-stone-300 dark:bg-slate-900`}
       >
         {children}
       </body>
