23edc9dfabcd8487185dcb033bffb3b76e14034e
Fu Jun <fulin.fujun@outlook.es>
2026-01-19 23:50:39 +0100

    feat: Create Articles section for POS app

--- frontend/app/pos/page.tsx
+++ frontend/app/pos/page.tsx
@@ -1,9 +1,9 @@
 import { fetchArticlesCategories } from "../lib/data";
+import ArticlesSection from "../ui/pos-interface/articles_section/ArticlesSection";
 import CategorySection from "../ui/pos-interface/categories_section/CategorySection";
 
 export default async function Page() {
   const categories = await fetchArticlesCategories();
-
   return (
     <section
       id="pos-interface"
@@ -24,7 +24,9 @@ export default async function Page() {
       </div>
       <div className="col-start-5 col-end-7 row-start-1 row-end-4 xl:row-end-3 bg-stone-300 rounded border-s border-stone-300"></div>
       <div className="col-start-5 col-end-7 row-start-4 xl:row-start-3 row-end-6 bg-stone-300 rounded border-s border-t border-stone-300"></div>
-      <div className="col-start-3 col-end-5 row-start-4 row-end-6 bg-stone-300 m-2 rounded overflow-y-scroll"></div>
+      <div className="col-start-3 col-end-5 row-start-4 row-end-6 bg-stone-300 m-2 rounded overflow-y-scroll">
+        <ArticlesSection />
+      </div>
       <div className="bg-grey-300 col-start-7 col-end-8 row-start-1 row-end-6 bg-stone-100 py-2 flex flex-col justify-between border-s border-stone-300"></div>
       <div className="row-start-6 row-end-7 bg-stone-100 col-start-1 col-end-8 border-t border-stone-300"></div>
     </section>
