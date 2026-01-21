6d46fd019037359d39f5348b8b4c174fa28e56ca
Fu Jun <fulin.fujun@outlook.es>
2026-01-21 23:41:42 +0100

    feat: add function to articles button wich add new receipt line inside receipt lines table.
    If the article is added, plus 1 the quantity of the article clicked.

--- frontend/app/ui/pos-interface/articles_section/ArticleButton.tsx
+++ frontend/app/ui/pos-interface/articles_section/ArticleButton.tsx
@@ -1,24 +1,24 @@
+"use client";
 import { article } from "@/app/lib/types/types";
+import { usePsGlobalContext } from "../context/PsGlobalContext";
 
 export default function ArticleButton({ article }: { article: article }) {
+  const { handleNewReceiptLine } = usePsGlobalContext();
+
   return (
     <>
       <div
         key={article.id}
         title={article.name}
-        className="grid grid_rows_5 bg-stone-100 size-20 md:size-24 xl:size-30 cursor-pointer rounded shadow"
+        className="grid grid-rows-5 items-center bg-stone-100 size-20 md:size-24 xl:size-30 cursor-pointer rounded shadow"
+        onClick={() => handleNewReceiptLine(article)}
       >
-        <div
-          className="row-start-2 row-end-6 flex flex-col"
-          // onClick={() => handleNewArticleLine(article)}
-        >
-          <h3 className="text-base xl:text-3xl text-center font-semibold text-red-600">
-            {article.cod_art}
-          </h3>
-          <p className="uppercase text-wrap text-sm text-center text-blue-950">
-            {article.name}
-          </p>
-        </div>
+        <h3 className="row-span-2 text-base xl:text-3xl text-center font-semibold text-red-600">
+          {article.cod_art}
+        </h3>
+        <p className="row-span-3 uppercase text-wrap text-sm text-center text-blue-950">
+          {article.name}
+        </p>
       </div>
     </>
   );
