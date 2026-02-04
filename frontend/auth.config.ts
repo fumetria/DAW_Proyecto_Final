e60a0a8e8b1454310ea9695d163e34721cce264d
Fu Jun <fulin.fujun@outlook.es>
2026-02-04 23:44:33 +0100

    feat: delete unused code

--- frontend/auth.config.ts
+++ frontend/auth.config.ts
@@ -9,20 +9,6 @@ export const authConfig = {
             const isLoggedIn = !!auth?.user;
             const pathname = nextUrl.pathname;
             const protectedURLs = pathname.startsWith('/dashboard') || pathname.startsWith('/pos');
-            // const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-            // const isOnPos = nextUrl.pathname.startsWith('/pos');
-            // if (isOnDashboard) {
-            //     if (isLoggedIn) return true;
-            //     return false;
-            // } else if (isLoggedIn) {
-            //     return Response.redirect(new URL('/dashboard', nextUrl));
-            // }
-            // if (isOnPos) {
-            //     if (isLoggedIn) return true;
-            //     return false;
-            // } else if (isLoggedIn) {
-            //     return Response.redirect(new URL('/pos', nextUrl))
-            // }
 
             if (protectedURLs) {
                 return isLoggedIn;
