c8953a8682acc7e43899d938c0d84736236a459e
Fu Jun <fulin.fujun@outlook.es>
2026-02-05 17:10:48 +0100

    feat: delete old migration

--- frontend/drizzle/0001_simple_manta.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE "receipts-lines" DROP CONSTRAINT "receipts-lines_receipt_id_receipts_id_fk";
---> statement-breakpoint
-ALTER TABLE "receipts" DROP CONSTRAINT "receipts_user_id_users_id_fk";
---> statement-breakpoint
-ALTER TABLE "receipts-lines" ALTER COLUMN "receipt_id" SET DATA TYPE varchar;--> statement-breakpoint
-ALTER TABLE "receipts" ALTER COLUMN "user_id" SET DATA TYPE varchar;--> statement-breakpoint
-ALTER TABLE "receipts-lines" ADD CONSTRAINT "receipts-lines_receipt_id_receipts_num_receipt_fk" FOREIGN KEY ("receipt_id") REFERENCES "public"."receipts"("num_receipt") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "receipts" ADD CONSTRAINT "receipts_user_id_users_email_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("email") ON DELETE no action ON UPDATE no action;
