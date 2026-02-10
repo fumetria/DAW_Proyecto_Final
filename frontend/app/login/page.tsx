75b69c522eca843a9a2527c49ae8f8dc443cb644
Fu Jun <fulin.fujun@outlook.es>
2026-02-10 08:57:15 +0100

    style: improve responsive UI in login page

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -8,16 +8,16 @@ export default function Login() {
   return (
     <>
       <main>
-        <div className="grid grid-cols-6 w-screen h-screen">
-          <div className="col-span-4 bg-linear-to-b from-blue-100 to-blue-300 grid justify-items-center items-center">
+        <div className="grid md:grid-cols-6 w-screen h-screen">
+          <div className="hidden md:grid col-span-4 bg-linear-to-br from-blue-100 to-blue-500 justify-items-center items-center">
             <div className="text-center">
               <div className="flex justify-center mb-2.5">
-                <Image src="/vercel.svg" alt="" width={30} height={30} />
-                <h1 className="text-stone-100 font-bold text-5xl">
-                  Bsness App
+                {/* <Image src="/vercel.svg" alt="" width={30} height={30} /> */}
+                <h1 className="text-stone-100 font-semibold text-5xl">
+                  BsnessApp
                 </h1>
               </div>
-              <p className="text-2xl font-extralight uppercase">
+              <p className="text-2xl text-stone-200 font-extralight uppercase">
                 Gestión de facturación en la nube
               </p>
             </div>
