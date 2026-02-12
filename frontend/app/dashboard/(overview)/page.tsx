9a941c074e1af10e0333f202ab622d976373c18c
Fu Jun <fulin.fujun@outlook.es>
2026-02-12 20:49:30 +0100

    feat: create loading animation when fetching data in dashboard

--- frontend/app/dashboard/(overview)/page.tsx
+++ frontend/app/dashboard/(overview)/page.tsx
@@ -1,19 +1,17 @@
-
-import { Metadata } from 'next';
+import { Metadata } from "next";
 import {
   fetchDashboardStats,
   fetchRecentReceipts,
   fetchMonthlyRevenue,
-} from '@/app/lib/data';
-import StatsCards from '@/app/ui/dashboard/stats-cards';
-import RecentTickets from '@/app/ui/dashboard/recent-tickets';
-import RevenueChart from '@/app/ui/dashboard/revenue-chart';
-import { Suspense } from 'react';
+} from "@/app/lib/data";
+import StatsCards from "@/app/ui/dashboard/stats-cards";
+import RecentTickets from "@/app/ui/dashboard/recent-tickets";
+import RevenueChart from "@/app/ui/dashboard/revenue-chart";
+import { Suspense } from "react";
 import { robotoFlex } from "@/app/fonts";
-// import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
 
 export const metadata: Metadata = {
-  title: 'Dashboard Home',
+  title: "Dashboard Home",
 };
 
 export default async function Page() {
@@ -23,21 +21,26 @@ export default async function Page() {
 
   return (
     <main>
-      <h1 className={`${robotoFlex.className} text-2xl 2xl:text-4xl font-semibold text-stone-500 dark:text-slate-50 mb-3`}>
+      <h1
+        className={`${robotoFlex.className} text-2xl 2xl:text-4xl font-semibold text-stone-500 dark:text-slate-50 mb-3`}
+      >
         Vista general
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <StatsCards
-          totalTickets={totalTickets}
-          totalRevenue={totalRevenue}
-        />
+        <Suspense>
+          <StatsCards totalTickets={totalTickets} totalRevenue={totalRevenue} />
+        </Suspense>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <div className="md:col-span-4 lg:col-span-4">
-          <RevenueChart revenue={monthlyRevenue} />
+          <Suspense>
+            <RevenueChart revenue={monthlyRevenue} />
+          </Suspense>
         </div>
         <div className="md:col-span-4 lg:col-span-4">
-          <RecentTickets latestReceipts={recentReceipts} />
+          <Suspense>
+            <RecentTickets latestReceipts={recentReceipts} />
+          </Suspense>
         </div>
       </div>
     </main>
