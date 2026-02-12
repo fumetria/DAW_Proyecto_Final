98eccb2c9f694ae820b5300e397139fc6eefb6c8
Fu Jun <fulin.fujun@outlook.es>
2026-02-12 20:50:23 +0100

    feat: delete unused code

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Image from "next/image";
 import LoginForm from "../ui/login/login-form";
 import { Suspense } from "react";
 
@@ -12,7 +11,6 @@ export default function Login() {
           <div className="hidden md:grid col-span-4 bg-linear-to-br from-blue-100 to-blue-500 justify-items-center items-center">
             <div className="text-center">
               <div className="flex justify-center mb-2.5">
-                {/* <Image src="/vercel.svg" alt="" width={30} height={30} /> */}
                 <h1 className="text-stone-100 font-semibold text-5xl">
                   BsnessApp
                 </h1>
