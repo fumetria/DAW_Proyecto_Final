e877c6de29ed37b4b9dd3c37660cd29c9df6a9fa
Fu Jun <fulin.fujun@outlook.es>
2026-02-13 23:19:19 +0100

    style: improve mobile resolutions UI
    - create a mobile table
    - improve responsive components

--- frontend/app/ui/dashboard/articles/table.tsx
+++ frontend/app/ui/dashboard/articles/table.tsx
@@ -9,7 +9,65 @@ export default async function ArticlesTable({
 }) {
   return (
     <>
-      <section>
+      {/* mobile table */}
+      <section className="md:hidden">
+        <table className="bg-stone-100 dark:text-slate-50 dark:bg-slate-800 min-w-full rounded-xl">
+          <thead className="rounded-lg">
+            <tr className="border-stone-300 border-b dark:border-slate-900">
+              <th scope="col" className="px-2 py-3 text-center">
+                Cod.
+              </th>
+              <th scope="col" colSpan={2} className="p-3 text-center ">
+                Descripción
+              </th>
+              <th scope="col" className="p-3 text-center">
+                Precio
+              </th>
+              <th scope="col" className="p-3 text-center">
+                Activo
+              </th>
+              <th scope="col" className="p-3 text-center">
+                Acciones
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {articles?.map((article) => {
+              const price = article.articlePvp
+                .toPrecision(3)
+                .toString()
+                .replace(".", ",");
+              return (
+                <tr
+                  key={article.articleID}
+                  className="border-b border-stone-300 last:border-none  dark:border-slate-900 hover:dark:bg-cyan-600 dark:hover:font-semibold"
+                >
+                  <td className="px-2 py-3 text-center">
+                    {article.articleCOD}
+                  </td>
+                  <td className="py-3 flex flex-col text-sm">
+                    <p>{article.articleName.toLocaleUpperCase()}</p>
+                    <p className="text-sm my-1 text-slate-400 uppercase italic font-light">
+                      {article.articleCategory}
+                    </p>
+                  </td>
+                  <td className="p-3 text-center uppercase"></td>
+                  <td className="p-3 text-center">{price}€</td>
+                  <td className="p-3 text-center">
+                    <IsActiveArticle article={article} />
+                  </td>
+                  <td className="py-3 flex justify-center gap-2 items-center">
+                    <UpdateArticle id={article.articleID} />
+                    <DeleteArticle id={article.articleID} />
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </section>
+      {/* desktop table */}
+      <section className="hidden md:block">
         <table className="bg-stone-100 dark:text-slate-50 dark:bg-slate-800 min-w-full rounded-xl">
           <thead className="rounded-lg">
             <tr className="border-stone-300 border-b dark:border-slate-900">
