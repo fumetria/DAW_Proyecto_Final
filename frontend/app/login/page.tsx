60e8d9b8a2f9e782aee7d428073890173cacad25
Fu Jun <fulin.fujun@outlook.es>
2026-02-16 23:15:24 +0100

    style: create dark theme for login page

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -7,25 +7,48 @@ export default function Login() {
   return (
     <>
       <main>
-        <div className="grid md:grid-cols-6 w-screen h-screen">
-          <div className="hidden md:grid col-span-4 bg-linear-to-br from-blue-100 to-blue-500 justify-items-center items-center">
-            <div className="text-center">
-              <div className="flex justify-center mb-2.5">
-                <h1 className="text-stone-100 font-semibold text-5xl">
-                  BsnessApp
-                </h1>
+        <section className="dark:hidden">
+          <div className="grid md:grid-cols-6 w-screen h-screen">
+            <div className="hidden md:grid col-span-4 bg-linear-to-br from-blue-100 to-blue-500 justify-items-center items-center">
+              <div className="text-center">
+                <div className="flex justify-center mb-2.5">
+                  <h1 className="text-stone-100 font-semibold text-5xl">
+                    BsnessApp
+                  </h1>
+                </div>
+                <p className="text-2xl text-stone-200 font-extralight uppercase">
+                  Gestión de facturación en la nube
+                </p>
               </div>
-              <p className="text-2xl text-stone-200 font-extralight uppercase">
-                Gestión de facturación en la nube
-              </p>
+            </div>
+            <div className="login-bg col-span-2 grid justify-items-center items-center ">
+              <Suspense>
+                <LoginForm />
+              </Suspense>
             </div>
           </div>
-          <div className="login-bg col-span-2 grid justify-items-center items-center ">
-            <Suspense>
-              <LoginForm />
-            </Suspense>
+        </section>
+        <section className="hidden dark:block">
+          <div className="grid md:grid-cols-6 w-screen h-screen">
+            <div className="hidden md:grid col-span-4 bg-linear-to-b from-slate-800 to-slate-900 justify-items-center items-center">
+              <div className="text-center">
+                <div className="flex justify-center mb-2.5">
+                  <h1 className="text-stone-100 dark:text-slate-200 font-semibold text-5xl">
+                    BsnessApp
+                  </h1>
+                </div>
+                <p className="text-2xl text-stone-200 dark:text-slate-200 font-extralight uppercase">
+                  Gestión de facturación en la nube
+                </p>
+              </div>
+            </div>
+            <div className="dark:bg-slate-950 col-span-2 grid justify-items-center items-center ">
+              <Suspense>
+                <LoginForm />
+              </Suspense>
+            </div>
           </div>
-        </div>
+        </section>
       </main>
     </>
   );
--- frontend/app/ui/login/login-form.tsx
+++ frontend/app/ui/login/login-form.tsx
@@ -16,25 +16,28 @@ export default function LoginForm() {
   const callbackUrl = searchParams.get("callbackUrl") || "/dashboard";
   const [errorMessage, formAction, isPending] = useActionState(
     authenticate,
-    undefined
+    undefined,
   );
 
   return (
     <form
       action={formAction}
-      className="bg-stone-200 rounded-xl p-8 shadow-2xl "
+      className="bg-stone-200 dark:bg-slate-800 rounded-xl p-8 shadow-2xl "
     >
-      <div className="mb-4 flex items-center justify-center">
+      <div className="mb-6 flex items-center justify-center">
         <Image src="/logo2.svg" width={60} height={60} alt="App logo" />
       </div>
       <div className="my-1 grid">
-        <label htmlFor="email" className="text-black font-semibold">
+        <label
+          htmlFor="email"
+          className="text-black dark:text-slate-100 mb-2 font-semibold"
+        >
           User
         </label>
-        <div className="text-base flex items-center border-2 rounded-lg py-1 ps-1 gap-2 focus-within:border-2 focus-within:border-blue-500">
+        <div className="text-base flex items-center dark:bg-slate-900 border-2 rounded-lg py-1 ps-1 gap-2 focus-within:border-2 dark:border-slate-500 focus-within:border-blue-500 dark:focus-within:border-cyan-500">
           <FontAwesomeIcon
             icon={faUser}
-            className="text-stone-400"
+            className="text-stone-400 dark:text-slate-500"
             width={20}
           />
           <input
@@ -42,18 +45,21 @@ export default function LoginForm() {
             id="email"
             name="email"
             placeholder="Introduce tu email"
-            className=" text-stone-900 outline-none border-none "
+            className=" text-stone-900 dark:text-slate-300 dark:placeholder:text-slate-500 outline-none border-none "
           />
         </div>
       </div>
       <div className="my-1 grid">
-        <label htmlFor="password" className="text-black font-semibold">
+        <label
+          htmlFor="password"
+          className="text-black font-semibold dark:text-slate-100 mb-2 "
+        >
           Password
         </label>
-        <div className="text-base flex items-center border-2 rounded-lg py-1 ps-1 gap-2 focus-within:border-2 focus-within:border-blue-500">
+        <div className="text-base flex items-center dark:bg-slate-900 border-2 rounded-lg py-1 ps-1 gap-2 focus-within:border-2 dark:border-slate-500 focus-within:border-blue-500 dark:focus-within:border-cyan-500">
           <FontAwesomeIcon
             icon={faLock}
-            className="text-stone-400"
+            className="text-stone-400 dark:text-slate-500"
             width={18}
           />
           <input
@@ -61,14 +67,14 @@ export default function LoginForm() {
             id="password"
             placeholder="Introduce contraseña"
             name="password"
-            className=" text-stone-900 outline-none border-none "
+            className=" text-stone-900 outline-none border-none dark:text-slate-300 dark:placeholder:text-slate-500 "
           />
         </div>
       </div>
       <div className="mt-5 grid items-center justify-items-center">
         <input type="hidden" name="redirectTo" value={callbackUrl} />
         <button
-          className="px-2 py-1 bg-blue-500 rounded border border-transparent text-stone-100 hover:text-blue-500 hover:bg-blue-200 hover:border-blue-500 cursor-pointer"
+          className="px-2 py-1 bg-blue-500 dark:bg-cyan-500 rounded border border-transparent text-stone-100 hover:text-blue-500 hover:bg-blue-200 hover:border-blue-500 dark:hover:bg-cyan-200 dark:hover:text-cyan-500 dark:hover:border-cyan-500 cursor-pointer"
           aria-disabled={isPending}
         >
           Login
