b12acfbeccb0ded55c3e32ae0797f53dde3b2693
Fu Jun <fulin.fujun@outlook.es>
2026-02-23 11:25:26 +0100

    style: add another box in card skeleton

--- frontend/app/ui/dashboard/skeletons.tsx
+++ frontend/app/ui/dashboard/skeletons.tsx
@@ -23,6 +23,13 @@ export function CardDashboardSkeleton() {
           </div>
           <p className="truncate rounded-xl bg-white px-4 py-8 text-center text-2xl font-medium dark:bg-slate-900 dark:text-slate-50 animate-pulse"></p>
         </div>
+        <div className="rounded-xl bg-gray-50 p-2 shadow-sm dark:bg-slate-800 w-70">
+          <div className="flex p-4">
+            <div className="h-5 w-5 bg-slate-400 animate-pulse rounded-full"></div>
+            <h3 className="ml-2 bg-slate-400 w-30 rounded-xl animate-pulse"></h3>
+          </div>
+          <p className="truncate rounded-xl bg-white px-4 py-8 text-center text-2xl font-medium dark:bg-slate-900 dark:text-slate-50 animate-pulse"></p>
+        </div>
       </div>
     </>
   );
