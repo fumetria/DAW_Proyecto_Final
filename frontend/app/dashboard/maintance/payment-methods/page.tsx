09113cb00eaa63699849975703821e6f8e157f24
Fu Jun <fulin789@gmail.com>
2026-03-26 08:52:25 +0100

    feat: create payment-methods maintance page

---
+++ frontend/app/dashboard/maintance/payment-methods/page.tsx
@@ -0,0 +1,45 @@
+import PaymentMethodsTable from "@/app/ui/dashboard/payment-methods/table";
+import { Metadata } from "next";
+import { robotoFlex } from "@/app/fonts";
+import Search from "@/app/ui/search";
+import CreatePaymentMethodAction from "@/app/ui/dashboard/payment-methods/createPaymentMethodAction";
+import Pagination from "@/app/ui/components/Pagination";
+import { fetchFilteredPaymentMethods } from "@/app/lib/data";
+
+export const metadata: Metadata = {
+  title: "Dashboard - Maintance payment methods ",
+};
+
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+    page?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || "";
+  const currentPage = Number(searchParams?.page) || 1;
+  const { paymentMethods, totalCount } = await fetchFilteredPaymentMethods(
+    query,
+    currentPage,
+  );
+  return (
+    <>
+      <section className="w-full">
+        <div className="mb-5">
+          <h1
+            className={`${robotoFlex.className} text-2xl 2xl:text-4xl font-semibold text-stone-500 dark:text-slate-50`}
+          >
+            Mantenimiento artículos
+          </h1>
+        </div>
+        <div className="mb-6 flex gap-1 xl:gap-3">
+          <Search placeholder="Introduce palabra a buscar" />
+          <CreatePaymentMethodAction />
+        </div>
+        <PaymentMethodsTable paymentMethods={paymentMethods} />
+        <Pagination totalCount={totalCount} currentPage={currentPage} />
+      </section>
+    </>
+  );
+}
